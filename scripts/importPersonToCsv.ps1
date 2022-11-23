79c9efead7e8537b7cd97c79ec9c3e836cc5d77a
Ethan Zeronik <ejzeronik@gmail.com>
2022-11-23 05:42:27 -0500

    person table and data done

--- scripts/importPersonToCsv.ps1
+++ scripts/importPersonToCsv.ps1
@@ -1,53 +1,26 @@
 <#
-NOTE: creates the master CSV for transactions to import into the database
+NOTE: creates the master CSV for biofile to import into the database
 
 pulls automatically from retrosheet:
-https://www.retrosheet.org/transactions/readme.txt
+https://www.retrosheet.org/biofile.htm
 
 will clean up everything but the output csv
 #>
 
-# make all the column names
-$transactionColumnHeaders =
-'PrimaryDate',
-'Time',
-'PrimaryApprox',
-'SecondaryDate',
-'SecondaryApprox',
-'TransactionId',
-'PlayerId',
-'Type',
-'FromTeam',
-'FromLeague',
-'ToTeam',
-'ToLeague',
-'DraftType',
-'DraftRound',
-'PickNumber',
-'OtherInformation';
-
-$zipPath = '../in/TRANs.zip';
-$transactionPath = '../in/transactions';
-$transactionExport = '../out/exportTRAN.csv';
+$dataPath = '../in/BIOs.txt';
+$transactionExport = '../out/exportBIO.csv';
 
 # then use a rest call to get the zip file
-Invoke-RestMethod -Uri 'https://www.retrosheet.org/transactions/tranDB.zip' -Method Get -ContentType 'application/zip' -OutFile $zipPath;
-
-# then expand the transactions
-Expand-Archive -Path $zipPath -DestinationPath $transactionPath;
+Invoke-RestMethod -Uri 'https://www.retrosheet.org/BIOFILE.TXT' -Method Get -ContentType 'application/text' -OutFile $dataPath;
 
 # import all csvs from the folder
-$importedData = Get-ChildItem ($transactionPath + '/tran.txt') | Select-Object -ExpandProperty FullName | Import-CSV -Header $transactionColumnHeaders;
-
-# then replace every property with "Approx" in the property name
-$formattedData = $importedData | Select-Object -ExcludeProperty '*Approx' -Property '*', @{Name = 'PrimaryDateIsApproximate'; Expression = { $_.PrimaryApprox -replace "@", 1 } }, @{Name = 'SecondaryDateIsApproximate'; Expression = { $_.SecondaryApprox -replace "@", 1 } };
+$importedData = Get-ChildItem $dataPath | Select-Object -ExpandProperty FullName | Import-CSV;
 
 # finally export it to a csv
-$formattedData | Export-Csv -Path $transactionExport -Delimiter ',' -NoTypeInformation;
+$importedData | Export-Csv -Path $transactionExport -Delimiter ',' -NoTypeInformation;
 
 # delete the zip and the input files
-Remove-Item $transactionPath -Recurse -Force;
-Remove-Item $zipPath -Recurse -Force
+Remove-Item $dataPath -Recurse -Force
 
 # let us know
 Write-Output 'Done with game log conversion';
--- tables/createPersonTable.sql
+++ tables/createPersonTable.sql
@@ -1,35 +1,52 @@
-CREATE TABLE PERSON(
-    RetrosheetID                    varchar2(8) NOT NULL PRIMARY KEY,
-    LastName                        varchar2(30),
-    FirstName                       varchar2(20),
-    UseName                         varchar2(30),
-    BirthDate                       date,
-    BirthCity                       varchar2(50),
-    BirthState                      varchar2(2),
-    BirthCountry                    varchar2(30),
-    DebutDateAsPlayer               date,
-    DateOfLastGameAsPlayer          date,
-    DebutDateAsManager              date,
-    DateOfLastGameAsManager         date,
-    DebutDateAsCoach                date,
-    DateOfLastGameAsCoach           date,
-    DebutDateAsUmpire               date,
-    DateOfLastGameAsUmpire          date,
-    DeathDate                       date,
-    DeathCity                       varchar2(50),
-    DeathState                      varchar2(2),
-    DeathCountry                    varchar2(30),
-    Bat                             varchar2(1),
-    Throws                          varchar2(1),
-    Height                          number(2),
-    Weight                          number(3),
-    Cemetery                        varchar2(30),
-    CemeteryCity                    varchar2(50),
-    CemeteryState                   varchar2(2),
-    CemeteryCountry                 varchar2(30),
-    CemeteryNote                    varchar2(250),
-    BirthName                       varchar2(50),
-    NameChange                      varchar2(100),
-    BatChange                       varchar2(100),
-    HOF                             varchar2(100)
+/* 
+NOTE: creates the table for transactions
+*/
+
+-- drop the old table
+DROP TABLE person CASCADE CONSTRAINTS;
+
+-- make the table schema
+CREATE TABLE person (
+    playerid VARCHAR2 (8) NOT NULL,
+    lastname VARCHAR2 (30),
+    firstname VARCHAR2 (50),
+    usename VARCHAR2 (30),
+    birthdate date,
+    birthcity VARCHAR2 (50),
+    birthstate VARCHAR2 (30),
+    birthcountry VARCHAR2 (30),
+    debutdateasplayer date,
+    dateoflastgameasplayer date,
+    debutdateasmanager date,
+    dateoflastgameasmanager date,
+    debutdateascoach date,
+    dateoflastgameascoach date,
+    debutdateasumpire date,
+    dateoflastgameasumpire date,
+    deathdate date,
+    deathcity VARCHAR2 (50),
+    deathstate VARCHAR2 (50),
+    deathcountry VARCHAR2 (30),
+    bat VARCHAR2 (1),
+    throws VARCHAR2 (1),
+    height VARCHAR2 (6),
+    weight NUMBER (3),
+    cemetery VARCHAR2 (50),
+    cemeterycity VARCHAR2 (50),
+    cemeterystate VARCHAR2 (30),
+    cemeterycountry VARCHAR2 (30),
+    cemeterynote VARCHAR2 (250),
+    birthname VARCHAR2 (50),
+    namechange VARCHAR2 (100),
+    batchange VARCHAR2 (100),
+    hof VARCHAR2 (100)
 );
+
+-- add the PK to the table
+ALTER TABLE person ADD CONSTRAINT person_pk PRIMARY KEY (playerid);
+
+-- create ref
+CREATE OR REPLACE PUBLIC SYNONYM person FOR admin.person;
+
+-- after that commit all of the changes made
+COMMIT;
